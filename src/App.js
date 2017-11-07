a35b9d61212ee040d54edbd3d9d24db10048ab47
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-11-07 21:01:41 +0100

    Create app & Title component

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Title from './components/Title'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Title content="Hangman" />
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
